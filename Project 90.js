66074832470da52ac32d9cf18bdb3e2eecdc81cc
Aevmanya <78971052+Aevmanya@users.noreply.github.com>
2021-12-18 10:30:16 +0530

    Add files via upload

---
+++ Project 90.js
@@ -0,0 +1,9 @@
+function adduser(){
+player_1_name= document.getElementById("player1_name_input").value;
+player_2_name= document.getElementById("player2_name_input").value;
+    
+localStorage.setItem("Player_1_name", player_1_name);
+localStorage.setItem("Player_2_name", player_2_name);
+    
+window.location= "quiz_game_page.html";
+}
